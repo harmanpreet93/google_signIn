b7a2a7fc9d0358134dc6e37dea3a6e06d30cd240
Harmanpreet Singh <cs12b1017@iith.ac.in>
2016-03-17 15:10:15 +0530

    google signin for android

---
+++ app/README.md
@@ -0,0 +1,2 @@
+
+This project implements google signin for android
---
+++ app/src/main/java/practice/google_signin/SignInActivity.java
@@ -0,0 +1,149 @@
+package practice.google_signin;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class SignInActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
+
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        configureSignIn();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                Log.v(TAG, "SignIn clicked");
+                findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+                showProgress(true);
+                signIn();
+                break;
+
+        }
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        showAlertDialogBox();
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            if(acct != null) {
+                Log.v(TAG, "User Info: " + acct.getDisplayName() + " email: " + acct.getEmail());
+            }
+            Log.v(TAG, "signIn successful");
+            openNewActivity(MainActivity.class);
+
+        } else {
+            showAlertDialogBox();
+            // Signed out, show unauthenticated UI.
+        }
+    }
+
+    private void configureSignIn() {
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // If you need to request additional scopes to access Google APIs, specify them with requestScopes.
+
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setScopes(gso.getScopeArray());
+        signInButton.setOnClickListener(this);
+    }
+
+    private void customizeSignInButton(GoogleSignInOptions gso) {
+
+    }
+
+    private void openNewActivity(Class className) {
+        Intent intent = new Intent(this,className);
+        startActivity(intent);
+        finish();
+    }
+
+    private void showAlertDialogBox() {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("SignIn Error");
+        alertDialog.setMessage("Error in Google Signin, please try again");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+                    }
+                });
+        alertDialog.setCancelable(false);
+        alertDialog.show();
+        showProgress(false);
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    private void showProgress(final boolean show) {
+        View mProgressView = findViewById(R.id.login_progress);
+        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+}
